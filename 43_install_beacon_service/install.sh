0f6deae980a715e90af8f9eb983e4ead19be1faf
ruphin <ruphin@ruphin.net>
2022-06-05 02:43:20 +0200

    Use local eth1 endpoint on beacon node

--- 43_install_beacon_service/install.sh
+++ 43_install_beacon_service/install.sh
@@ -13,7 +13,7 @@ done
 
 sed -i "s/\[network\]/${network}/" "${DIR}/beacon.service.tmp"
 
-printf "\nEnter eth1 endpoint (https://mainnet.infura.io/v3/1234567890abcdef1234567890abcdef)\n> "
+printf "\nEnter backup eth1 endpoint (https://mainnet.infura.io/v3/1234567890abcdef1234567890abcdef)\nOr leave blank to use only local\n> "
 read eth1
 
 sed -i 's|\[eth1\]|'"${eth1}"'|' "${DIR}/beacon.service.tmp"
