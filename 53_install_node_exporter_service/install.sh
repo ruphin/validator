c08af18da8fe205793e592013c6dfdbee01ed42f
ruphin <ruphin@ruphin.net>
2022-06-07 23:42:31 +0200

    Fix service name

--- 53_install_node_exporter_service/install.sh
+++ 53_install_node_exporter_service/install.sh
@@ -3,9 +3,9 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-cp "${DIR}/geth.service" /etc/systemd/system/geth.service
+cp "${DIR}/node_exporter.service" /etc/systemd/system/node_exporter.service
 
 systemctl daemon-reload
-systemctl stop geth
-systemctl start geth
-systemctl enable geth
+systemctl stop node_exporter
+systemctl start node_exporter
+systemctl enable node_exporter
