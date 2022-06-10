94ab27f1fc389c3896f4e9ba797e3f026c1e30cb
ruphin <ruphin@ruphin.net>
2022-06-10 04:18:08 +0200

    Add prometheus config for geth and lighthouse

--- 58_install_grafana/install.sh
+++ 58_install_grafana/install.sh
@@ -5,7 +5,7 @@ wget -q -O - https://packages.grafana.com/gpg.key | apt-key add -
 echo "deb https://packages.grafana.com/oss/deb stable main" > /etc/apt/sources.list.d/grafana.list
 
 apt update
-apt install grafana
+apt install -y grafana
 
 systemctl start grafana-server
 systemctl enable grafana-server
