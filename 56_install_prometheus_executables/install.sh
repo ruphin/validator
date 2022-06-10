b5c2b7cf6ca3b4fa67384de2edec4f9a6b1fc889
ruphin <ruphin@ruphin.net>
2022-06-10 04:23:51 +0200

    Make script idempotent

--- 56_install_prometheus_executables/install.sh
+++ 56_install_prometheus_executables/install.sh
@@ -6,9 +6,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 mv /tmp/prometheus /usr/local/bin/
 mv /tmp/promtool /usr/local/bin/
 
-mkdir -p /etc/prometheus
-mv /tmp/consoles /etc/prometheus/
-mv /tmp/console_libraries /etc/prometheus/
+mkdir -p /etc/prometheus/consoles
+mv /tmp/consoles/* /etc/prometheus/consoles/
+mkdir -p /etc/prometheus/console_libraries
+mv /tmp/console_libraries/* /etc/prometheus/console_libraries/
 
 cp "${DIR}/prometheus.yml" /etc/prometheus/
 
