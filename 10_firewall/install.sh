d5be42e1ddacdf250a8b9b546ef8d9b52aa2cc5f
ruphin <ruphin@ruphin.net>
2022-06-10 04:03:31 +0200

    Add grafana

--- 10_firewall/install.sh
+++ 10_firewall/install.sh
@@ -9,6 +9,9 @@ ufw allow 9000/udp
 ufw allow 30303/tcp
 ufw allow 30303/udp
 
+# Open port for Grafana on local network only
+sudo ufw allow from 192.168.0.0/24 to any port 3000
+
 printf "Allow remote access through ssh?\n"
 PS3="> "
 select ssh in "yes" "no"; do
---
+++ 58_install_grafana/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+wget -q -O - https://packages.grafana.com/gpg.key | apt-key add -
+echo "deb https://packages.grafana.com/oss/deb stable main" > /etc/apt/sources.list.d/grafana.list
+
+apt update
+apt install grafana
+
+systemctl start grafana-server
+systemctl enable grafana-server
