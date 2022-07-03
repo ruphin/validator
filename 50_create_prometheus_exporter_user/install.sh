d0d007a4c6f207cfc46e71339046418c4bce35f9
ruphin <ruphin@ruphin.net>
2022-07-03 04:09:56 +0200

    Fix prometheus_exporter user

--- 50_create_prometheus_exporter_user/install.sh
+++ 50_create_prometheus_exporter_user/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-useradd --no-create-home --shell /bin/false ethereum_metrics_exporter || true # ignore error if user already exists
+useradd --no-create-home --shell /bin/false prometheus_exporter || true # ignore error if user already exists
